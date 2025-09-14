33bf6986231f61076169229690124b92a334a859
Damola <adebitandamola02@gmail.com>
2025-09-14 16:42:43 +0100

    fixed all bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Comment from "./Component/Comment";
-import SendCommentReply from "./Component/SendCommentReply"
-import SendReply from "./Component/SendReply";
-import ReplyFormat from "./Component/ReplyFormat";
-import data from "./data.json"
-import { nanoid } from 'nanoid';
+import Comment from "./components/Comment";
+import SendCommentReply from "./components/SendCommentReply";
+import SendReply from "./components/SendReply";
+import ReplyFormat from "./components/ReplyFormat";
+import data from "./data.json";
+import { nanoid } from "nanoid";
 
-function App() { 
+function App() {
   const [text, setText] = useState("");
   const [replyContent, setReplyContent] = useState("");
   const [activeComment, setActiveComment] = useState(null);
@@ -14,161 +14,247 @@ function App() {
   const [allData, setAllData] = useState(
     () => JSON.parse(localStorage.getItem("comments")) || []
   );
-    
+
   const [sendData, setSendData] = useState(
     () => JSON.parse(localStorage.getItem("sendData")) || []
   );
 
-  useEffect(() => {
-    localStorage.setItem('comments', JSON.stringify(allData))
-  }, [allData])
+  // Track user votes for each comment/reply
+  const [userVotes, setUserVotes] = useState(
+    () => JSON.parse(localStorage.getItem("userVotes")) || {}
+  );
 
   useEffect(() => {
-    localStorage.setItem('sendData', JSON.stringify(sendData))
-  }, [sendData])
-
+    localStorage.setItem("comments", JSON.stringify(allData));
+  }, [allData]);
 
   useEffect(() => {
-    setAllData(data.comments)
-  }, [])
+    localStorage.setItem("sendData", JSON.stringify(sendData));
+  }, [sendData]);
+
+  useEffect(() => {
+    localStorage.setItem("userVotes", JSON.stringify(userVotes));
+  }, [userVotes]);
+
+  useEffect(() => {
+    setAllData(data.comments);
+  }, []);
+
+  // Voting functionality
+  const handleVote = (commentId, replyId, voteType) => {
+    const voteKey = replyId ? `${commentId}-${replyId}` : commentId;
+    const currentVote = userVotes[voteKey];
+
+    // If user already voted this way, do nothing (ignore multiple clicks)
+    if (currentVote === voteType) {
+      return;
+    }
+
+    let newVote = null;
+    let scoreChange = 0;
+
+    if (!currentVote) {
+      // First time voting
+      newVote = voteType;
+      scoreChange = voteType === "up" ? 1 : -1;
+    } else {
+      // User had opposite vote, change it
+      newVote = voteType;
+      scoreChange = voteType === "up" ? 2 : -2; // Remove old vote and add new vote
+    }
+
+    setUserVotes((prev) => ({
+      ...prev,
+      [voteKey]: newVote,
+    }));
+
+    if (replyId) {
+      // Update reply score
+      setAllData((prevData) =>
+        prevData.map((comment) =>
+          comment.id === commentId
+            ? {
+                ...comment,
+                replies: comment.replies.map((reply) =>
+                  reply.id === replyId
+                    ? { ...reply, score: reply.score + scoreChange }
+                    : reply
+                ),
+              }
+            : comment
+        )
+      );
+    } else {
+      // Update comment score in allData
+      setAllData((prevData) =>
+        prevData.map((comment) =>
+          comment.id === commentId
+            ? { ...comment, score: comment.score + scoreChange }
+            : comment
+        )
+      );
+
+      // Update comment score in sendData
+      setSendData((prevData) =>
+        prevData.map((comment) =>
+          comment.id === commentId
+            ? { ...comment, score: comment.score + scoreChange }
+            : comment
+        )
+      );
+    }
+  };
+
+  // Edit reply functionality
+  const updateReply = (commentId, replyId, newContent) => {
+    setAllData((prevData) =>
+      prevData.map((comment) =>
+        comment.id === commentId
+          ? {
+              ...comment,
+              replies: comment.replies.map((reply) =>
+                reply.id === replyId ? { ...reply, content: newContent } : reply
+              ),
+            }
+          : comment
+      )
+    );
+  };
 
   //ADD REPLIES TO COMMENTS
-
   const addReply = (commentId, replyContent, replyingTo) => {
     const newReply = {
       id: nanoid(),
-      replyingTo: replyingTo ,
+      replyingTo: replyingTo,
       content: replyContent,
       createdAt: new Date().toLocaleString(),
       score: 0,
       user: {
-        image: { 
+        image: {
           png: "./images/avatars/image-juliusomo.png",
         },
-        username: "juliusomo"
-      }
-    }
+        username: "juliusomo",
+      },
+    };
 
-    setAllData(prevComment => {
-      return prevComment.map(comment => {
+    setAllData((prevComment) => {
+      return prevComment.map((comment) => {
         if (comment.id === commentId) {
           return {
             ...comment,
-            replies: 
-            [
-              ...comment.replies, newReply ]
+            replies: [...comment.replies, newReply],
           };
         }
         return comment;
-      })
-    })
+      });
+    });
 
-    setActiveComment(false)
-    
-  }
-  
-  //ADD NEW COMMENT 
+    setActiveComment(false);
+  };
 
+  //ADD NEW COMMENT
   const addComment = () => {
-    setSendData(prev => {
-      return[
+    setSendData((prev) => {
+      return [
         ...prev,
-         {
+        {
           id: nanoid(),
           content: text,
           createdAt: new Date().toLocaleString(),
           score: 0,
           user: {
-            image: { 
+            image: {
               png: "./images/avatars/image-juliusomo.png",
-              webp: "./images/avatars/image-juliusomo.webp"
+              webp: "./images/avatars/image-juliusomo.webp",
             },
-            username: "juliusomo"
+            username: "juliusomo",
           },
-          replies: []
-        }
-      ]
-    })
-  }
+          replies: [],
+        },
+      ];
+    });
+  };
 
   // Delete Comment
-
   const deleteComment = (id) => {
-    setSendData(prevData => prevData.filter(item => item.id !== id));
-  }
+    setSendData((prevData) => prevData.filter((item) => item.id !== id));
+  };
 
   // Delete ReplyComment
   const deleteReplyComment = (commentId, replyId) => {
     const updatedComment = [...allData].map((item) => {
       if (item.id === commentId) {
-        item.replies = item.replies.filter((reply) => reply.id !== replyId)
+        item.replies = item.replies.filter((reply) => reply.id !== replyId);
       }
-      return item
-    })
+      return item;
+    });
 
-    setAllData(updatedComment)
-  }
+    setAllData(updatedComment);
+  };
 
-  
-  const itemData = allData.map(data => {
+  const itemData = allData.map((data) => {
     return (
-      <div className="container" key = {data.id}>
+      <div className="container" key={data.id}>
         <Comment
-          id = {data.id}
-          name = {data.user.username}
-          date = {data.createdAt}
-          content = {data.content}
-          image = {data.user.image.png}
-          score = {data.score}
-          replyData = {data.replies}
-          setActiveComment = {setActiveComment}
-          deleteReplyComment = {deleteReplyComment}
+          id={data.id}
+          name={data.user.username}
+          date={data.createdAt}
+          content={data.content}
+          image={data.user.image.png}
+          score={data.score}
+          replyData={data.replies}
+          setActiveComment={setActiveComment}
+          deleteReplyComment={deleteReplyComment}
+          handleVote={handleVote}
+          userVotes={userVotes}
+          updateReply={updateReply}
         />
 
-      {activeComment && activeComment.id === data.id && (
-        <SendCommentReply 
-          id = {data.id}
-          replyTo = {data.user.username}
-          onReply = {addReply}
-          replyContent = {replyContent}
-          setReplyContent = {setReplyContent}
-        />
-      )}
+        {activeComment && activeComment.id === data.id && (
+          <SendCommentReply
+            id={data.id}
+            replyTo={data.user.username}
+            onReply={addReply}
+            replyContent={replyContent}
+            setReplyContent={setReplyContent}
+          />
+        )}
       </div>
-    )
-
-  })
+    );
+  });
 
   return (
-    <div className='box'>
+    <div className="box">
       {itemData}
 
-      {sendData.length > 0 && sendData.map((data) => {
-        return (
-          <ReplyFormat 
-            key = {data.id}
-            id = {data.id} 
-            name = {data.user.username}
-            date = {data.createdAt}
-            text = {data.content}
-            image = {data.user.image.png}
-            score = {data.score}
-            sendData = {sendData}
-            setSendData = {setSendData}
-            deleteComment = {deleteComment}
-          /> 
-        )
-      })}
+      {sendData.length > 0 &&
+        sendData.map((data) => {
+          return (
+            <ReplyFormat
+              key={data.id}
+              id={data.id}
+              name={data.user.username}
+              date={data.createdAt}
+              text={data.content}
+              image={data.user.image.png}
+              score={data.score}
+              sendData={sendData}
+              setSendData={setSendData}
+              deleteComment={deleteComment}
+              handleVote={handleVote}
+              userVotes={userVotes}
+            />
+          );
+        })}
 
-      <SendReply 
-        handleSubmit= {addComment}
-        text = {text}
-        setText = {setText}
-        submitLabel = "SEND"
+      <SendReply
+        handleSubmit={addComment}
+        text={text}
+        setText={setText}
+        submitLabel="SEND"
       />
     </div>
   );
 }
-
 
 export default App;
